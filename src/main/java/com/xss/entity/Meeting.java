b09a0607b1a18701adcc9fbd504cda2b7c7fcbe2
Xie <1501879136@qq.com>
2020-07-01 19:44:34 +0800

    增加代码

---
+++ src/main/java/com/xss/entity/Meeting.java
@@ -0,0 +1,102 @@
+package com.xss.entity;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Meeting implements Serializable {
+  private static final long serialVersionUID= 1L;
+
+  private Integer id;
+  private String deptName;
+  private Integer deptId;
+  private String title;
+  private String content;
+  private Date publishDate;
+  private String startTime;
+  private String endTime;
+  private String status;
+  private String makeUser;
+
+
+
+  public String getDeptName() {
+    return deptName;
+  }
+
+  public void setDeptName(String deptName) {
+    this.deptName = deptName;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public Integer getDeptId() {
+    return deptId;
+  }
+
+  public void setDeptId(Integer deptId) {
+    this.deptId = deptId;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public Date getPublishDate() {
+    return publishDate;
+  }
+
+  public void setPublishDate(Date publishDate) {
+    this.publishDate = publishDate;
+  }
+
+  public String getStartTime() {
+    return startTime;
+  }
+
+  public void setStartTime(String startTime) {
+    this.startTime = startTime;
+  }
+
+  public String getEndTime() {
+    return endTime;
+  }
+
+  public void setEndTime(String endTime) {
+    this.endTime = endTime;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public String getMakeUser() {
+    return makeUser;
+  }
+
+  public void setMakeUser(String makeUser) {
+    this.makeUser = makeUser;
+  }
+}
